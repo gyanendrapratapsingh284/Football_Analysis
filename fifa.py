de4c1b2c75c9fdf713754b9e5e9f9abfff20fa37
gyanendrapratapsingh284 <97172671+gyanendrapratapsingh284@users.noreply.github.com>
2023-08-19 07:35:44 +0530

    Update fifa.py

--- fifa.py
+++ fifa.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import plotly.express as px
-from typing import Union, Optional, Literal
 from streamlit_lottie import st_lottie
 import json
 # df_club = pd.read_csv("C:\\Users\\Gyanendra\\Desktop\\files\\clubs.csv")
